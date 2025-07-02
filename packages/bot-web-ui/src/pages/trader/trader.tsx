410d066b2aba1d0d588cc9dad6bdb88f53428b13
Brian <ndanumumo93@gmail.com>
2025-07-03 02:31:20 +0300

    mumo

--- packages/bot-web-ui/src/pages/trader/trader.tsx
+++ packages/bot-web-ui/src/pages/trader/trader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './finesttool.module.scss'; // assuming you renamed it correctly
+import styles from './trader.module.scss'; // assuming you renamed it correctly
 
 const Finesttool = () => {
     return (
